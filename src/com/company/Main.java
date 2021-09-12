6265c02f67a88a8dbe039a313da32303fa77a1ea
Hasnain <hasnainajmal281@gmail.com>
2021-09-12 12:24:39 +0500

    Initial commit

---
+++ src/com/company/Main.java
@@ -0,0 +1,16 @@
+package com.company;
+
+public class Main {
+
+    public static void main(String[] args) {
+	// write your code here
+        System.out.print("The sum of num1 and num2 is: ");
+        int num1 = 34;
+        int num2 = 56;
+        int sum = num1 + num2;
+        System.out.print(sum);
+    }
+}
+//Add two Number
+// AddTwoNumbers      -Pascal Naming Convention  used for Class
+//         addTwoNumber  -camel Case naming convetion   used for function
